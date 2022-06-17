1c143894aa4bccd8b01a9306cc2a56a40f5c6bf9
Eetu Hyyryläinen <eetu.hyyrylainen@siilim190901.local>
2022-06-17 15:10:23 +0300

    Add exercises 3.1-3.6

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# full-stack-part3
-Full Stack Part3
+Init project:
+>npm init
+Run the project:
+>node index.js
+Or, if defined in scripts ("start": "node index.js",):
+>npm start
+ Define a project dependency
+ >npm install express
+ Define dev dependency for watching changes
+ >npm install --save-dev nodemon
+ Define script: "dev": "nodemon index.js",
+ >npm run dev
---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Phonebook backend</h1>')
+})
+
+app.get('/info', (req, res) => {
+    console.log(req);
+  res.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>
+  `)
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 999999)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'Missing name or number'
+    })
+  } else if (persons.find(p => p.name === body.name)) {
+    return response.status(400).json({ 
+        error: 'name must be unique'
+      })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
